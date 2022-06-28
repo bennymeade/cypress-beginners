667509e2f63e93f9f67efd0704e780efa27c05dc
Snowman315 <shiva.sud07@gmail.com>
2022-06-28 16:52:53 +0100

    Planet and Society verification

--- cypress/integration/unilever/homepage.test.js
+++ cypress/integration/unilever/homepage.test.js
@@ -5,6 +5,10 @@ describe('Homepage', () => {
     cy.visit('/');
     cy.get('[data-testid="uco-c-header-logo-link"]').should('be.visible');
   });
+  it('Verify that site Planet and Society is visible', () => {
+    cy.visit('/');
+    cy.get('[data-testid="uco-c-header-menu-link"]').should('be.visible');
+  });
   it('Verify change location page', () => {
     cy.visit('/');
     cy.get('[data-testid="uol-c-link-label"]').contains('Change location').should('be.visible').click();
