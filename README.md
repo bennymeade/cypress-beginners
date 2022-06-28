832c3366367a01cee60cbe26a3fe0d5cef4ec928
bennymeade <brendan_meade@yahoo.co.uk>
2022-06-28 14:37:19 +0100

    test: added homepage logo test

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # cypress-beginners
+
+Cypress docs here: https://docs.cypress.io/guides/overview/why-cypress
+
+Web application status codes: https://en.wikipedia.org/wiki/List_of_HTTP_status_codes
+
---
+++ cypress/integration/unilever/homepage.test.js
@@ -0,0 +1,6 @@
+describe('Homepage', () => {
+  it('Verify that site logo is visible', () => {
+    cy.visit('/');
+    cy.get('[data-testid="uco-c-header-logo-link"]').should('be.visible');
+  });
+});
